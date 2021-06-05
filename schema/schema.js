9cff468df5b7d27ae024c9ad1384b8d56a73ca5b
shreya-sridhar <shreyamohak@outlook.com>
2021-06-04 23:02:26 -0700

    added rootquery

---
+++ schema/schema.js
@@ -0,0 +1,39 @@
+const graph = require('graphql')
+const {
+    GraphQLObjectType,
+    GraphQLInt,
+    GraphQLString
+} = graphql 
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id:{type:GraphQLString},
+        firstName:{type:GraphQLString},
+        age:{type:GraphQLInt}
+    }
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields:{
+        user:{
+            type: UserType,
+            args: {id:{ type: GraphQLString}},
+            resolve(parentValue,args){
+                
+            }
+        }
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
